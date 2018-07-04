38fd1e2f79c6d7f99f3930dc1126597c6f5bf9d2
C0ns7ruc7 <00093215@student.rocleiden.nl>
2018-07-04 11:23:01 +0200

    login gegenereerd
    authenticatie gegenereerd met [php artisan make:auth].
    routes aangepast

--- ValiantHekert/routes/web.php
+++ ValiantHekert/routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', function () {
 });
 Route::resource("/berichten", "BerchtenController");
 Route::resource("/gebrukers", "GebruikerController");
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
